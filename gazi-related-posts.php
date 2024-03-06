9f9b55da3ff2c92a055886e7f3cf478213f8f38e
Gazi Akter <gaziakterdev@gmail.com>
2024-03-06 19:12:04 +0600

    setup abspath and other

---
+++ gazi-related-posts.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       Gazi Related Posts
+ * Plugin URI:        https://gaziakter/plugin/gazi-related-posts/
+ * Description:       Gazi Related Posts for WordPress offers you the ability to link related posts to each other with just 1 click!
+ * Version:           1.0.0
+ * Author:            Gazi Akter
+ * Author URI:        https://gaziakter.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       gazi-post
+ * Domain Path:       /languages
+ */
+
+ if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
